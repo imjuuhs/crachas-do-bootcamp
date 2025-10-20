125bac204f5b330a4426536a7da09fae6788f989
xtheusx <matheus99793@hotmail.com>
2025-10-20 20:56:24 -0300

    minha versão melhorada dos crachás

--- meu-cracha-dapp/src/config.ts
+++ meu-cracha-dapp/src/config.ts
@@ -1,5 +1,5 @@
 // Configuração dos contratos e objetos do sistema de crachás
-export const PACKAGE_ID = "0xaf115340f88fc6bd1105088ceb2cfb581798047b1a8078546b422cd0d1aa4894";
+export const PACKAGE_ID = "0x87922a85fdf955533ceac0b717700270de83cb58cf1b1e355bcd4b2900eb78f5";
 
 export const CRACHA_ID = "0x98721795985337b8935078b27e5c5d98a3b96210f1d24a96192ae490c64465c1"; // Substitua pelo ID real do crachá
 
@@ -8,4 +8,5 @@ export const NETWORK = "mainnet";
 
 //upgrade cap = 0xe8da14068d6c02b6cf3fdae9fb158c15480807f4b05ef2b364b4cb360146fdda
 
-export const UPGRADE_CAP = "0x66b5b0213b016fe0fa04e2cdd009972e9e366fd98de83fbaa47fef1d478930b5";
+export const UPGRADE_CAP = "0xb30bd654b7f761913354bc027837f8ddbd875a9854ec44826a383eaaf81f46f5";
+
